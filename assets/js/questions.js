e4e7819e77c5c9fb7b3cd6c7f93b0120c498b5e2
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2021-10-06 12:16:41 -0700

    added questions for quiz as well as declared inital variables for JS page

---
+++ assets/js/questions.js
@@ -0,0 +1,34 @@
+// Array of questions to be looped through during quiz
+var questions = [
+
+    {
+        question: 'Which of the below is NOT a JavaScript data type?',
+        choices: ['Number', 'String', 'Line', 'Boolean'],
+        correct: 'Line'
+
+    },
+    {
+        question: 'What does the === operator mean?',
+        choices: ['Loosely equal', 'Strictly equal', 'Loosely does not equal', 'Strictly does not equal'],
+        correct: 'Stricyly equal'
+
+    },
+    {
+        question: 'Which of the below is NOT a looping structure in JavaScript?',
+        choices: ['For', 'While', 'Do-while', 'Calculate'],
+        correct: 'Calculate'
+
+    },
+    {
+        question: 'Which of the below is NOT a definition for an undefined value in JavaScript?',
+        choices: ['Variable used in the code does not exist', 'Variable is not assigned to any value', 'Property does not exist', 'Property has been sold'],
+        correct: 'Property has been sold'
+
+    },
+    {
+        question: 'Which of the below is NOT a type of pop up box available in JavaScript?',
+        choices: ['Alert', 'Confirm', 'Prompt', 'Ask'],
+        correct: 'Ask'
+
+    }
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,25 @@
-var questions = [
+// Declaring variables
+var scoresEl = document.querySelector('#scores');
+var timerEl = document.querySelector('#timer');
+var questionsEl = document.querySelector('#questionOptions');
+var startEl = document.querySelector('#startBtn');
+var endEl = document.querySelector('#end');
+var finalScoreEl = document.querySelector('#finalScore');
+var initialsEl = document.querySelector('#initials');
+var submitEl = document.querySelector('#submit');
 
-    {
-        question: 'whats your fav color',
-        choices: ['red', 'greeg', 'blue'],
-        correct: 'red'
+startEl.addEventListener('click', function () {
 
-    },
-    {
-        question: 'whats your fav color',
-        choices: ['red', 'greeg', 'blue'],
-        correct: 'red'
+    console.log('Start Button Clicked!');
+    displayQuestion()
 
-    }
+});
 
-]
-
-function displayQuestion() {
+function displayQuestion () {
 
     var title = document.createElement('h1')
 
-    title.innerHTML = questions[0].question
+    title.innerHTML = questions[0].questions
 
     document.getElementById('title').appendChild(title)
 
@@ -42,6 +42,5 @@ displayQuestion();
 function choiceClicked () {
 
     console.log('choice got clicked')
-    
 
 }
