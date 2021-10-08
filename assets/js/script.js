7b102ea542d49b74851147695ee12544a5198b64
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2021-10-07 18:39:30 -0700

    added to the javascript loop that is displaying questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,55 @@
 // Declaring variables
-var scoresEl = document.querySelector('#scores');
-var timerEl = document.querySelector('#timer');
-var questionsEl = document.querySelector('#questionOptions');
-var startEl = document.querySelector('#startBtn');
-var endEl = document.querySelector('#end');
-var finalScoreEl = document.querySelector('#finalScore');
-var initialsEl = document.querySelector('#initials');
-var submitEl = document.querySelector('#submit');
+var mainEl = document.querySelector('#mainBox')
+var h1El = document.querySelector('#h1')
+var p1El = document.querySelector('#p1')
+var scoresEl = document.querySelector('#scores')
+var timerEl = document.querySelector('#timer')
+var questionsEl = document.querySelector('#questionOptions')
+var startEl = document.querySelector('#startBtn')
+var endEl = document.querySelector('#end')
+var finalScoreEl = document.querySelector('#finalScore')
+var initialsEl = document.querySelector('#initials')
+var submitEl = document.querySelector('#submit')
+var questionNumber = 0
+
 
 startEl.addEventListener('click', function () {
 
-    console.log('Start Button Clicked!');
-    displayQuestion()
+    console.log('start button clicked!');
+    displayQuestion();
 
-});
+})
 
-function displayQuestion () {
+function displayQuestion() {
+    // clears text from main screen and hides begin button
+    h1El.innerHTML = ''
+    p1El.innerHTML = ''
+    startEl.classList.add('hide')
+    // loop to display questions
+    let quizQuestion = document.createElement('h1')
+        quizQuestion.textContent = questions[questionNumber].question
+        mainEl.appendChild(quizQuestion)
+    
+        // loop to display answers
+        for (let j = 0; j < questions[questionNumber].choices.length; j++) {
+            let quizAnswer = document.createElement('li')
+            quizAnswer.textContent = questions[questionNumber].choices[j]
+            quizAnswer.addEventListener('click', function() {
 
-    var title = document.createElement('h1')
+                choiceClicked(questions[questionNumber].choices[j])
 
-    title.innerHTML = questions[0].questions
-
-    document.getElementById('title').appendChild(title)
-
-    for (let i = 0; i < questions[0].choices.length; i++) {
-
-        var choice = document.createElement('button')
-
-        choice.innerHTML = choice[0].choices[i]
-
-        choice.addEventListener('click', choiceClicked)
-
-        document.getElementById('choices').appendChild(choice)
-
-    }
-
+            })
+            quizQuestion.appendChild(quizAnswer)
+        }
+    // }
+    console.log('function worked')
 }
 
-displayQuestion();
+function choiceClicked(event) {
 
-function choiceClicked () {
-
-    console.log('choice got clicked')
+    console.log('choice clicked', event)
+    //need to clear previous question content
+    questionNumber++
+    displayQuestion()
 
 }
